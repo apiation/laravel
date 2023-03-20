546b3e189474187d5172a8cee938827acc9f11b8
samirnijenhuis <samirnijenhuis@users.noreply.github.com>
2023-03-20 13:22:06 +0000

    Fix styling

--- config/apiation.php
+++ config/apiation.php
@@ -6,16 +6,15 @@ return [
     // Whether the API call should be made asynchronously
     'async' => true,
 
-//     To optimize performance you can queue the API call. To do so you can make an array and enter the desired queue and connection you want to push the job on.
-//        'queue' => [
-//        'queue' => '',
-//        'connection' => ''
-//    ],
-//     If you want to use the default queue settings you can do the following:
-//     'queue' => true,
-//     Alternatively, if you don't want to utilize the queue, leave the value null
+    //     To optimize performance you can queue the API call. To do so you can make an array and enter the desired queue and connection you want to push the job on.
+    //        'queue' => [
+    //        'queue' => '',
+    //        'connection' => ''
+    //    ],
+    //     If you want to use the default queue settings you can do the following:
+    //     'queue' => true,
+    //     Alternatively, if you don't want to utilize the queue, leave the value null
     'queue' => null,
-
 
     'sample_rate' => env('APIATION_SAMPLE_RATE', 0.03),
 ];
